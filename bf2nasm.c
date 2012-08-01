bbff8fb844209ff77c77f5e3088ab055b1b5d659
Star Brilliant <m13253@hotmail.com>
2012-08-01 22:54:55 +0800

    Implement some optimization

--- bf2nasm.c
+++ bf2nasm.c
@@ -392,6 +392,22 @@ void process(void)
                 registers.pedi = 0;
                 dump_buffer(2);
             }
+            else if(match("->+<]", 0))
+            {
+                fputs("\tmov\tal, byte [edi]\n\tadd\tbyte [edi+1], al\n\txor\teax, eax\n\tmov\tbyte [edi], al\n", stdout);
+                registers.known |= RG_EAX;
+                registers.changed &= ~RG_EAX;
+                registers.eax = 0;
+                dump_buffer(5);
+            }
+            else if(match("->+>+<<]", 0))
+            {
+                fputs("\tmov\tal, byte [edi]\n\tadd\tbyte [edi+1], al\n\tadd\tbyte [edi+2], al\n\txor\teax, eax\n\tmov\tbyte [edi], al\n", stdout);
+                registers.known |= RG_EAX;
+                registers.changed &= ~RG_EAX;
+                registers.eax = 0;
+                dump_buffer(8);
+            }
             else
             {
                 push_edi();
